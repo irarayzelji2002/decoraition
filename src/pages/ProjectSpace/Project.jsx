33522e8408f4e6481a372ae59ccbdd07c2fdc6df
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-11-27 01:29:54 +0800

    pin adjust

--- src/pages/ProjectSpace/Project.jsx
+++ src/pages/ProjectSpace/Project.jsx
@@ -68,6 +68,7 @@ import {
   dialogStyles,
   dialogTitleStyles,
 } from "../../components/RenameModal";
+import Error from "../../components/Error";
 
 function Project() {
   const { projectId } = useParams();

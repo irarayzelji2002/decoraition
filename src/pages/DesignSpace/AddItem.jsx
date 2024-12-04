3eb282ab9b58efbec2bd39a04069fc6c8162c1ee
Ira Rayzel S. Ji <irarayzelji@gmail.com>
2024-12-05 01:47:25 +0800

    2mb limit

--- src/pages/DesignSpace/AddItem.jsx
+++ src/pages/DesignSpace/AddItem.jsx
@@ -197,9 +197,9 @@ const AddItem = () => {
       message = "Please upload an image file of png, jpg, or jpeg type";
       showToast("error", message);
     } else {
-      const maxSize = 5 * 1024 * 1024; // 5MB in bytes
+      const maxSize = 2 * 1024 * 1024; // 2MB in bytes
       if (file.size > maxSize) {
-        message = "Image size must be less than 5MB";
+        message = "Image size must be less than 2MB";
         showToast("error", message);
       }
     }
@@ -257,9 +257,9 @@ const AddItem = () => {
         formErrors.image = "Please upload an image file of png, jpg, jpeg, gif, or webp type";
         showToast("error", formErrors.image);
       } else {
-        const maxSize = 5 * 1024 * 1024; // 5MB in bytes
+        const maxSize = 2 * 1024 * 1024; // 2MB in bytes
         if (selectedImage.size > maxSize) {
-          formErrors.image = "Image size must be less than 5MB";
+          formErrors.image = "Image size must be less than 2MB";
           showToast("error", formErrors.image);
         }
       }
